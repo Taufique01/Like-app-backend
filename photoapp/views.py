998d5029034090ea355516f3420a1d1c5b6a5ff4
taufique <taufiquehr@gmail.com>
2019-11-24 01:51:02 +0600

    add

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
